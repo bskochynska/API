0c7b295bebc26ad1303965019dc5f062efd0dc68
bskochynska <124739569+bskochynska@users.noreply.github.com>
2026-02-22 03:18:13 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import booking, utils, auth, user, session, genre, content, actor, cinema_hall
 from fastapi.middleware.cors import CORSMiddleware
-from app.db.session import engine 
-from app.models.base import Base 
+from app.db.session import engine
+from app.db.base import Base
 
 app = FastAPI(title="MovieHub API", version="1.0.0")
 
