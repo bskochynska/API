6170d247687ae436fb62fa312c9ffad932087a20
bskochynska <124739569+bskochynska@users.noreply.github.com>
2026-02-22 03:33:56 +0200

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from typing import AsyncGenerator
 
 DATABASE_URL = os.getenv(
-    DATABASE_URL,
     DATABASE_URL
 )
 
