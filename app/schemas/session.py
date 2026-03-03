0c8a298ffec5385be383d3477ced4355c18ede7e
bskochynska <124739569+bskochynska@users.noreply.github.com>
2026-03-03 21:52:51 +0200

    Update session.py

--- app/schemas/session.py
+++ app/schemas/session.py
@@ -1,23 +1,25 @@
 from pydantic import BaseModel
-from typing import Optional
 from datetime import datetime
 from app.schemas.content import ContentResponse
+from app.models.enums import SessionStatus
 
 class SessionBase(BaseModel):
     content_id: int
     cinema_hall_id: int
     start_time: datetime
     ticket_price: float
+    status: SessionStatus = SessionStatus.PLANNED  
 
 class SessionCreate(SessionBase):
     pass
 
 class SessionUpdate(BaseModel):
-    content_id: Optional[int] = None
-    cinema_hall_id: Optional[int] = None
-    start_time: Optional[datetime] = None
-    ticket_price: float
-
+    content_id: int | None = None
+    cinema_hall_id: int | None = None
+    start_time: datetime | None = None
+    ticket_price: float | None = None 
+    status: SessionStatus | None = None 
+    
 class SessionResponse(SessionBase):
     id: int
 
